f5c9aec8d4f9a781c6c1777b7171c1737177bd6c
BananaDuncan <b10121014@gmail.com>
2021-05-05 00:47:59 +0800

    Update lambda_function.py

--- PPEDetection/PPEDetection/lambda_function.py
+++ PPEDetection/PPEDetection/lambda_function.py
@@ -5,7 +5,7 @@ import config
 def lambda_handler(event, context):
     print(event)
     dataModel = event
-    ppeDetection =  PpeDetect(dataModel, config.aws_access_key_id, config.aws_secret_access_key, config.region_name,config.table,config.bucket)
+    ppeDetection =  PpeDetect(dataModel, config.aws_access_key_id, config.aws_secret_access_key, config.region_name,config.table)
     ppeDetection.query_config()
     ppeDetection.storeImage()
     ppeDetection.ppeDetect()
